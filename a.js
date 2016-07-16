4c9d527c834e18bd778dee361c42ffe56f3695f4
Vinnie <215903432@qq.com>
2016-07-16 17:10:36 +0800

    some new features demo

--- a.js
+++ a.js
@@ -1,31 +1,49 @@
-var array = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]];
-/**螺旋打印一个矩阵
- * @param arr {Array} 一个NxN的矩阵的二维数组表示
- */
-function showHelix(arr){
-    var startX = 0,
-        endX = arr.length - 1,
-        startY = 0,
-        endY = arr[0].length - 1,
-        i;
-        while(startX <= endX && startY <= endY){
-            for(i = startX; i <= endX; i++){
-                console.log(arr[startY][i]);    //输出上边的行
-            }
-            startY++;                           //行坐标加1
-            for(i = startY; i <= endY; i++){
-                console.log(arr[i][endX]);      //输出右边的列
-            }
-            endX--;                             // 列坐标减1
-            for(i = endX; i >= startX; i--){      // 输出下边的行
-                console.log(arr[endY][i])
-            }
-            endY--;                             // 行坐标减1
-            for(i = endY; i >= startY; i--){          // 输出左边的列
-                console.log(arr[i][startX]);
-            }
-            startX++;                           //列坐标增加1
-        }
+// var array = [
+//     [1, 2, 3, 4],
+//     [5, 6, 7, 8],
+//     [9, 10, 11, 12],
+//     [13, 14, 15, 16]
+// ];
+// /**螺旋打印一个矩阵
+//  * @param arr {Array} 一个NxN的矩阵的二维数组表示
+//  */
+// function showHelix(arr) {
+//     var startX = 0,
+//         endX = arr.length - 1,
+//         startY = 0,
+//         endY = arr[0].length - 1,
+//         i;
+//     while (startX <= endX && startY <= endY) {
+//         for (i = startX; i <= endX; i++) {
+//             console.log(arr[startY][i]); //输出上边的行
+//         }
+//         startY++; //行坐标加1
+//         for (i = startY; i <= endY; i++) {
+//             console.log(arr[i][endX]); //输出右边的列
+//         }
+//         endX--; // 列坐标减1
+//         for (i = endX; i >= startX; i--) { // 输出下边的行
+//             console.log(arr[endY][i])
+//         }
+//         endY--; // 行坐标减1
+//         for (i = endY; i >= startY; i--) { // 输出左边的列
+//             console.log(arr[i][startX]);
+//         }
+//         startX++; //列坐标增加1
+//     }
+// }
+
+// showHelix(array);
+
+function each(list, callback) {
+    for (var n = 0; n < list.length; n++) {
+        callback.call(list[n], n);
+    }
 }
 
-showHelix(array);
+
+var arr = ['a', 'b', 'c']
+
+each(arr, function(index) {
+    console.log(Object.keys(this));
+})
